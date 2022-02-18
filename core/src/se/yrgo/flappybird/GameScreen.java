08d889b48023748ce6bf99d3d13298a1d43985f8
AlexanderKaG <alexanderkarlssongoransson@hotmail.com>
2022-02-18 23:05:45 +0100

    Added constructor for GameScreen class using a FlappyBirdGame object as input

--- core/src/se/yrgo/flappybird/GameScreen.java
+++ core/src/se/yrgo/flappybird/GameScreen.java
@@ -1,5 +1,9 @@
 package se.yrgo.flappybird;
 
 public class GameScreen implements Screen {
-    
+    final FlappyBirdGame game;
+
+    public GameScreen(final FlappyBirdGame game) {
+        this.game = game;
+    }
 }
