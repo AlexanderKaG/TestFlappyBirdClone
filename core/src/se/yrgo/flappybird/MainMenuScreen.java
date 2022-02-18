8d253df99bc0e0fc3bed0d81b44d400d6a40622d
AlexanderKaG <alexanderkarlssongoransson@hotmail.com>
2022-02-18 16:31:28 +0100

    Added visuals for main menu screen

--- core/src/se/yrgo/flappybird/MainMenuScreen.java
+++ core/src/se/yrgo/flappybird/MainMenuScreen.java
@@ -1,4 +1,65 @@
 package se.yrgo.flappybird;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.ScreenUtils;
+
 public class MainMenuScreen implements Screen {
+    final FlappyBirdGame game;
+    OrthographicCamera camera;
+
+    public MainMenuScreen(final FlappyBirdGame game) {
+        this.game = game;
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 800);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0,0,0.2f,1);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+
+        game.batch.begin();
+        game.font.draw(game.batch, "Welcome to MyExtendedTestGame!!!", 300, 600);
+        game.font.draw(game.batch, "Tap anywhere to begin", 300, 500);
+        game.batch.end();
+
+        if (Gdx.input.isTouched()) {
+            game.setScreen(new GameScreen(game));
+            dispose();
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
