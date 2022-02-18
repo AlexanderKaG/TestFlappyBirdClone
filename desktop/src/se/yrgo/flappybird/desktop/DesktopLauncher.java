b39bb828eb109ebd262e5ee02d4e98a425b9acbb
AlexanderKaG <alexanderkarlssongoransson@hotmail.com>
2022-02-18 15:04:33 +0100

    Adding libGDX generated gradle files

---
+++ desktop/src/se/yrgo/flappybird/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package se.yrgo.flappybird.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import se.yrgo.flappybird.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
