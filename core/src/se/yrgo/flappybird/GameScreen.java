6c6eee78496d9a824d507ac8b0aa2660f91e0ffe
AlexanderKaG <alexanderkarlssongoransson@hotmail.com>
2022-02-18 23:13:48 +0100

    Added a camera

--- core/src/se/yrgo/flappybird/GameScreen.java
+++ core/src/se/yrgo/flappybird/GameScreen.java
@@ -1,6 +1,8 @@
 package se.yrgo.flappybird;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 
 public class GameScreen implements Screen {
@@ -9,12 +11,18 @@ public class GameScreen implements Screen {
     Texture player;
     Texture obstacle;
 
+    OrthographicCamera camera;
+
     public GameScreen(final FlappyBirdGame game) {
         this.game = game;
 
         //load the images for player and obstacles
         player = new Texture(Gdx.files.internal("brownsquare.png"));
         obstacle = new Texture(Gdx.files.internal("mariopipe.png"));
+
+        //create the camera
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 800);
 
         
     }
