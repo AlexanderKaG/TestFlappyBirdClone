ecb49a6aeadbbe4260cc3935475a19de10f724fd
AlexanderKaG <alexanderkarlssongoransson@hotmail.com>
2022-02-18 23:18:15 +0100

    Refactored a few variable names and added a Rectangle to logically represent the player

--- core/src/se/yrgo/flappybird/GameScreen.java
+++ core/src/se/yrgo/flappybird/GameScreen.java
@@ -4,25 +4,35 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
 
 public class GameScreen implements Screen {
     final FlappyBirdGame game;
 
-    Texture player;
-    Texture obstacle;
+    Texture playerImage;
+    Texture obstacleImage;
 
     OrthographicCamera camera;
+
+    Rectangle player;
 
     public GameScreen(final FlappyBirdGame game) {
         this.game = game;
 
         //load the images for player and obstacles
-        player = new Texture(Gdx.files.internal("brownsquare.png"));
-        obstacle = new Texture(Gdx.files.internal("mariopipe.png"));
+        playerImage = new Texture(Gdx.files.internal("brownsquare.png"));
+        obstacleImage = new Texture(Gdx.files.internal("mariopipe.png"));
 
         //create the camera
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 800);
+
+        //create a Rectangle to logically represent the player
+        player = new Rectangle;
+        player.x = 800/2-64/2;
+        player.y = 400;
+        player.width = 64;
+        player.height = 64;
 
         
     }
