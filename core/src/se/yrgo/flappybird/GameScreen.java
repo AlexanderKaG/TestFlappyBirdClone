b440f26639e4c2daa12c609518709ab9b9493b9e
AlexanderKaG <alexanderkarlssongoransson@hotmail.com>
2022-02-18 23:10:53 +0100

    loaded assets for player and obstacles

--- core/src/se/yrgo/flappybird/GameScreen.java
+++ core/src/se/yrgo/flappybird/GameScreen.java
@@ -1,9 +1,21 @@
 package se.yrgo.flappybird;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+
 public class GameScreen implements Screen {
     final FlappyBirdGame game;
 
+    Texture player;
+    Texture obstacle;
+
     public GameScreen(final FlappyBirdGame game) {
         this.game = game;
+
+        //load the images for player and obstacles
+        player = new Texture(Gdx.files.internal("brownsquare.png"));
+        obstacle = new Texture(Gdx.files.internal("mariopipe.png"));
+
+        
     }
 }
