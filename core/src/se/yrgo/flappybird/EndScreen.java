488915d2699c0f8ebbe42641ce8e04dcb3e8070c
AlexanderKaG <alexanderkarlssongoransson@hotmail.com>
2022-02-20 18:18:23 +0100

    Displays previous scores

--- core/src/se/yrgo/flappybird/EndScreen.java
+++ core/src/se/yrgo/flappybird/EndScreen.java
@@ -3,6 +3,7 @@ package se.yrgo.flappybird;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 
@@ -21,6 +22,10 @@ public class EndScreen implements Screen {
         scoreboard = new Array<>();
     }
 
+    private void addScoreToScoreboard() {
+        scoreboard.add(GameScreen.points);
+    }
+
     @Override
     public void show() {
 
@@ -33,8 +38,13 @@ public class EndScreen implements Screen {
         camera.update();
         game.batch.setProjectionMatrix(camera.combined);
 
+        addScoreToScoreboard();
+        int scoreboardLayout = 550;
         game.batch.begin();
         game.font.draw(game.batch, "Your Score: " + GameScreen.points, 300, 600);
+        for (Integer score : scoreboard) {
+            game.font.draw(game.batch, "Previous scores: " + score, 300, scoreboardLayout - 10);
+        }
         game.font.draw(game.batch, "Tap anywhere to restart", 300, 500);
         game.batch.end();
 
